fa51578594b2c61e2a4216fed86b395cb0bd4cfd
Allie Diorio <allie.diorio@gmail.com>
2022-02-15 15:34:17 -0500

    Started creating css grid

--- src/components/productDetailsAllie/overviewAllie.jsx
+++ src/components/productDetailsAllie/overviewAllie.jsx
@@ -22,47 +22,42 @@ class OverviewAllie extends React.Component {
 
     render() {
         return (
-            <div>
-                <h2>Overview Module Allie</h2>
-                <div>
+            <div class="overviewContainer">
+                <div class="gallery">
                     <b>Image Gallery</b>
                     {this.props.products.map((product, i) => (
                         <div onClick={() => this.selectedProduct(product)} key={i}>{product.name}</div>
                     ))}
                     <img src={this.props.image}></img>
                 </div>
-                <br></br>
-                <div><b>Star Rating</b></div>
-                <br></br>
-                <div>
+                <div class="ratings"><b>Star Rating</b></div>
+                <div class="category">
                     <div><b>Category</b></div>
                     <div>{this.props.currentProduct.name}</div>
                     <div>{this.props.currentStyle.name}</div>
                     <div>${this.props.price}</div>
                 </div>
-                <br></br>
-                <div>
+                <div class="style">
                     <b>Style > Selected Style</b>
                     {this.props.styles.map((style, i) => (
                         <div onClick={() => this.selectedStyle(style)} key={i}>{style.name}</div>
                     ))}
                 </div>
-                <br></br>
-                <div>
-                    <div>sizes</div>
-                    {this.props.skus.map((sku, i) => (
-                        <div key={i}>{sku.size}</div>
-                    ))}
+                <div class="selection">
+                    <div>
+                        <div>sizes</div>
+                        {this.props.skus.map((sku, i) => (
+                            <div key={i}>{sku.size}</div>
+                        ))}
+                    </div>
+                    <div>
+                        <div>quantity</div>
+                        {this.props.skus.map((sku, i) => (
+                            <div key={i}>{sku.quantity}</div>
+                        ))}
+                    </div>
                 </div>
-                <br></br>
-                <div>
-                    <div>quantity</div>
-                    {this.props.skus.map((sku, i) => (
-                        <div key={i}>{sku.quantity}</div>
-                    ))}
-                </div>
-                <br></br>
-                <div>
+                <div class="description">
                     <div><b>Descripiton</b></div>
                     <div>Slogan: {this.props.currentProduct.slogan}</div>
                     <div>Description: {this.props.currentProduct.description}</div>

414f1f2852b9bb427ba19de90f37e27f8724b449
John Bronson <jbronson@gmail.com>
2022-02-25 16:34:35 -0600

    hit deadline. Modal still not finished but it's as good as I can get it

--- src/components/relatedItems/ProductComparisonModal.jsx
+++ src/components/relatedItems/ProductComparisonModal.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import StarRating from '../StarRating.jsx'
+import Styled from 'styled-components'
 
 const ProductComparisonModal = (props) => {
   if (!props.show) {
     return null
   }
+
   return <div id="comparison-modal">
     <div id="modal-content">
       <div id="modal-header"><h2>Comparing</h2><button onClick={e => {
@@ -12,18 +14,20 @@ const ProductComparisonModal = (props) => {
       }
       }>Close</button></div>
       <div id="modal-body">
-      <div>⭐️</div>
-      {console.log('props in the modal:', props)}
-      <img src={props.itemCardData.picture} width="200" height="200"></img>
-      <div id="ItemCard_Name">{props.itemCardData.name}</div>
-      <div id="ItemCard_Category">{props.itemCardData.category}</div>
-      <div id="ItemCard_Price">{props.itemCardData.price}</div>
-      <StarRating size={16} rating={props.itemCardData.rating} />
-      <hr />
-      ohh buddy
-      <div>Price: {props.price}</div>
-      {console.log(props)}
-      <div><img src={props.currentProductImage} width="200" height="200"></img></div>
+        <div id="comparisonProductModalPane">
+          <img src={props.itemCardData.picture} width="450" height="450"></img>
+          <StarRating size={16} rating={props.itemCardData.rating} />
+          <div id="ItemCard_Name">Name: {props.itemCardData.name}</div>
+          <div id="ItemCard_Category">Category: {props.itemCardData.category}</div>
+          <div id="ItemCard_Price">Price: {props.itemCardData.price}</div>
+        </div>
+        <div id="currentProductModalPane">
+          <div><img src={props.currentProductImage} width="450" height="450"></img></div>
+          <div><StarRating size={16} rating={props.itemCardData.rating} /></div>
+          <div>Name: {props.currentProduct.name}</div>
+          <div>Price: {props.currentProductPrice}</div>
+          <div>Category: {props.currentProduct.category}</div>
+        </div>
       </div>
       <div id="modal-footer">Footer</div>
     </div>

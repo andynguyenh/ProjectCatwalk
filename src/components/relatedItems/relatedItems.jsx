d994ab82f8470c8963c5fd9aa9541914b77f96a3
Allie Diorio <allie.diorio@gmail.com>
2022-02-25 17:32:06 -0500

    Finsihed overview  Please enter the commit message for your changes. Lines starting

--- src/components/relatedItems/relatedItems.jsx
+++ src/components/relatedItems/relatedItems.jsx
@@ -12,7 +12,7 @@ class RelatedItems extends React.Component {
     render() {
         return (
             <div>
-                <h2>Related Products Module</h2>
+                {/* <h2>Related Products Module</h2> */}
                 <RelatedProductsCarousel currentProduct={this.props.currentProduct} relatedItems={this.props.relatedItems}/>
                 {/* <YourOutfitContainer /> */}
             </div>
